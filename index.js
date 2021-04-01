b914a06638f6468c4f159c08b10a3e5c2ba81f6d
igalv <vilenskyigal@gmail.com>
2021-04-01 02:58:15 +0200

    js practice

--- index.js
+++ index.js
@@ -35,3 +35,50 @@ function front22(str) {
 console.log(front22("kitten"));
 console.log(front22("Ha"));
 console.log(front22("abc"));
+
+/* Given a string, return true if the string starts with 'hi'and false otherwise.
+
+Examples
+
+startHi('hi there') → true
+startHi('hi') → true
+startHi('hello hi') → false */
+
+function startHi(str) {
+  let strToArr = str.split(" ");
+  let result;
+  if (strToArr[0] == "hi") {
+    result = true;
+  } else {
+    result = false;
+  }
+  return result;
+}
+
+console.log(startHi("hi there"));
+console.log(startHi("hi"));
+console.log(startHi("hello hi"));
+
+/* Given two temperatures, return true if one is less than 0 and the other is greater than 100.
+
+Examples
+
+icyHot(120, -1) → true
+icyHot(-1, 120) → true
+icyHot(2, 120) → false */
+
+function icyHot(temp1, temp2) {
+  let result;
+  if (temp1 > 100 && temp2 < 0) {
+    result = true;
+  } else if (temp2 > 100 && temp1 < 0) {
+    result = true;
+  } else {
+    result = false;
+  }
+  return result;
+}
+
+console.log(icyHot(120, -1));
+console.log(icyHot(-1, 120));
+console.log(icyHot(2, 120));
