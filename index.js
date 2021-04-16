c263432f0298f0645bce313ad2e73b318f320b5c
igalv <vilenskyigal@gmail.com>
2021-04-16 13:13:48 +0200

    js practice

--- index.js
+++ index.js
@@ -894,3 +894,39 @@ console.log("------------------");
 /* console.log(altPairs("kitten")); */
 console.log(altPairs("Chocolate"));
 console.log(altPairs("CodingHorror"));
+
+// Given a string name, e.g. "Bob", return a greeting of the form "Hello Bob!".
+
+// Examples
+
+// helloName('Bob') → Hello Bob!
+// helloName('Alice') → Hello Alice!
+// helloName('X') → Hello X!
+
+function helloName(name) {
+  let gree = "Hello";
+  let mark = "!";
+  return `${gree} ${name}${mark}`;
+}
+
+console.log("------------------");
+console.log(helloName("Bob"));
+console.log(helloName("Alice"));
+console.log(helloName("X"));
+
+// Given two strings, a and b, return the result of putting them together in the order abba, e.g. "Hi" and "Bye" returns "HiByeByeHi".
+
+// Examples
+
+// makeAbba('Hi', 'Bye') → HiByeByeHi
+// makeAbba('Yo', 'Alice') → YoAliceAliceYo
+// makeAbba('What', 'Up') → WhatUpUpWhat
+
+function makeAbba(a, b) {
+  return `${a}${b}${b}${a}`;
+}
+
+console.log("------------------");
+console.log(makeAbba("Hi", "Bye"));
+console.log(makeAbba("Yo", "Alice"));
+console.log(makeAbba("What", "Up"));
